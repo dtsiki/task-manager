48a505ae0314c40317012e3931d6a362cce73b65
thevioletmaniac <thevioletmaniac@gmail.com>
2021-07-23 16:45:56 +0600

    Adjust Button component

--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -5,17 +5,19 @@ import './style.scss';
 interface Props {
   children?: React.ReactNode;
   buttonClassName?: string;
-  onClick: () => void;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
   variant?: string,
+  icon?: React.ReactNode,
 }
 
-const Button: React.FC<Props> = ({ children, onClick, buttonClassName, variant }) => {
+const Button: React.FC<Props> = ({ children, onClick, buttonClassName, variant, icon }) => {
   return (
     <button
       onClick={onClick}
       className={`button button--${variant || 'primary'} ${buttonClassName ? ` ${buttonClassName}` : ''}`}
     >
       {children}
+      {icon && <span className="button__icon">{icon}</span>}
     </button>
   );
 };
